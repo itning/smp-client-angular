540cabe411a76ecd88da165494d20dea19647fec
itning <itning@itning.top>
2020-04-16 13:10:46 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'smp-client-angular';
+export class AppComponent implements OnInit {
+  isCollapsed = false;
+  logoImageSrc = 'assets/logo.png';
+
+  ngOnInit(): void {
+
+  }
+
+  onCollapsedChange() {
+    if (this.isCollapsed) {
+      this.logoImageSrc = 'assets/logo.png';
+    } else {
+      this.logoImageSrc = 'assets/logo-small.png';
+    }
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  onBtnClick(e) {
+    console.log(e);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import {
+  NzBreadCrumbModule, NzButtonModule, NzIconModule,
+  NzLayoutModule, NzMenuModule
+} from 'ng-zorro-antd';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -10,9 +23,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzIconModule,
+    NzMenuModule,
+    NzButtonModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
