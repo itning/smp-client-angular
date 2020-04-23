bd3e58b1be1b993bfa9e119695cf0eb49b2727a9
itning <itning@itning.top>
2020-04-23 20:49:34 +0800

    feat: show once toast

--- src/app/http/ResponseErrorInterceptor.ts
+++ src/app/http/ResponseErrorInterceptor.ts
@@ -11,26 +11,36 @@ import {SecurityService} from '../service/security.service';
  */
 @Injectable()
 export class ResponseErrorInterceptor implements HttpInterceptor {
+  private static IS_SHOW_NOTIFICATION_NOW = false;
 
   constructor(private notification: NzNotificationService,
               private securityService: SecurityService) {
   }
 
-  private handleError(notification: NzNotificationService, securityService: SecurityService) {
+  private showErrorNotificationOnceOnView(title: string, content: string, id: string) {
+    if (ResponseErrorInterceptor.IS_SHOW_NOTIFICATION_NOW) {
+      return;
+    } else {
+      this.notification.error(title, content, {nzKey: id})
+        .onClose.subscribe(() => ResponseErrorInterceptor.IS_SHOW_NOTIFICATION_NOW = false);
+    }
+  }
+
+  private handleError(securityService: SecurityService) {
     return (error: HttpErrorResponse) => {
       if (error.error instanceof ErrorEvent) {
         // 发生客户端或网络错误。
         console.error('An error occurred:', error.error.message);
-        notification.error('客户端错误：', error.error.message);
+        this.showErrorNotificationOnceOnView('客户端错误：', error.error.message, 'client');
       } else {
         console.error(
           `Backend returned code ${error.status}, ` +
           `body was: ${JSON.stringify(error.error)}`);
         if (error.status === 0) {
-          notification.error('网络错误', '请检查网络连接后再试');
+          this.showErrorNotificationOnceOnView('网络错误：', '请检查网络连接后再试', 'no-net');
         } else {
           const errorMsg = (error.error as RestModel<string>).msg;
-          notification.error('错误', errorMsg);
+          this.showErrorNotificationOnceOnView('错误：', errorMsg, 'backend');
           if (error.status === 401) {
             securityService.route2Login();
           }
@@ -43,7 +53,7 @@ export class ResponseErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
       .pipe(
-        catchError(this.handleError(this.notification, this.securityService)),
+        catchError(this.handleError(this.securityService)),
       );
   }
 }
