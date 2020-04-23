bb8cf862a2d0898a29ea1c078f41a1295533f0c6
itning <itning@itning.top>
2020-04-23 21:25:25 +0800

    build: reconstruction

--- src/app/module/statistics/component/statistics/statistics.component.ts
+++ src/app/module/statistics/component/statistics/statistics.component.ts
@@ -34,18 +34,56 @@ export class StatisticsComponent implements OnInit, OnDestroy {
   echartsInstances: echarts.ECharts[] = [];
   datePipe = new DatePipe('zh-Hans');
   intervalNumber: number;
+  dateRange: Date[] = [];
+  nowDay = dayjs();
+  lastMonthFromNow = dayjs().subtract(1, 'month');
+  counselorAllChartOption: echarts.EChartOption = {
+    title: {text: '各辅导员统计信息'},
+    tooltip: {trigger: 'axis', axisPointer: {type: 'shadow'}},
+    legend: {data: ['寝室归寝', '课堂出勤', '请假']},
+    grid: {left: '3%', right: '4%', bottom: '3%', containLabel: true},
+    xAxis: {type: 'value', boundaryGap: [0, 0.01]},
+    yAxis: {type: 'category'},
+    series: [{name: '寝室归寝', type: 'bar', data: []}, {name: '课堂出勤', type: 'bar', data: []}, {name: '请假', type: 'bar', data: []}]
+  };
+  polylineOption: echarts.EChartOption = {
+    title: {text: '统计折线图'},
+    tooltip: {trigger: 'axis'},
+    dataZoom: [{type: 'slider', start: 0, end: 100}, {type: 'inside', start: 0, end: 100}],
+    legend: {data: ['寝室归寝', '课堂出勤', '请假']},
+    grid: {left: '3%', right: '4%', bottom: '3%', containLabel: true},
+    xAxis: {type: 'category', boundaryGap: false, data: []},
+    yAxis: {type: 'value'},
+    series: [
+      {name: '寝室归寝', type: 'line', smooth: true, data: []},
+      {name: '课堂出勤', type: 'line', smooth: true, data: []},
+      {name: '请假', type: 'line', smooth: true, data: []}
+    ]
+  };
+  counselorEcharts: echarts.ECharts;
+  polylineEcharts: echarts.ECharts;
 
   constructor(private statisticsService: StatisticsService) {
+    this.dateRange.push(this.lastMonthFromNow.toDate(), this.nowDay.toDate());
   }
 
   ngOnInit(): void {
+    this.counselorEcharts = echarts.init(this.counselorElementRef.nativeElement);
+    this.polylineEcharts = echarts.init(this.polylineElementRef.nativeElement);
+    this.echartsInstances.push(this.counselorEcharts, this.polylineEcharts);
+
     this.initLiveChart();
     this.getApartmentInfoChart();
-    this.getCounselorAllChart(dayjs().subtract(1, 'month').format('YYYY-MM-DD'), dayjs().format('YYYY-MM-DD'));
-    this.getPolylineData(dayjs().subtract(1, 'month'), dayjs());
+    this.getCounselorAllChart(this.lastMonthFromNow.format('YYYY-MM-DD'), this.nowDay.format('YYYY-MM-DD'));
+    this.getPolylineData(this.lastMonthFromNow, this.nowDay);
+
+    window.onresize = () => {
+      this.echartsInstances.forEach((item) => item.resize());
+    };
   }
 
   ngOnDestroy(): void {
+    window.onresize = null;
     if (this.intervalNumber) {
       window.clearInterval(this.intervalNumber);
     }
@@ -56,17 +94,17 @@ export class StatisticsComponent implements OnInit, OnDestroy {
 
     const comingOption = {
       tooltip: {formatter: '{a} <br/>{b} : {c}%'},
-      series: [{name: '归寝', type: 'gauge', detail: {formatter: '{value}%'}, data: [{value: 0, name: '归寝率'}]}]
+      series: [{name: '', type: 'gauge', detail: {formatter: '{value}%'}, data: [{value: 0, name: '实时归寝率'}]}]
     };
 
     const leaveOption = {
       tooltip: {formatter: '{a} <br/>{b} : {c}%'},
-      series: [{name: '请假', type: 'gauge', detail: {formatter: '{value}%'}, data: [{value: 0, name: '请假率'}]}]
+      series: [{name: '', type: 'gauge', detail: {formatter: '{value}%'}, data: [{value: 0, name: '实时请假率'}]}]
     };
 
     const classComingOption = {
       tooltip: {formatter: '{a} <br/>{b} : {c}%'},
-      series: [{name: '出勤', type: 'gauge', detail: {formatter: '{value}%'}, data: [{value: 0, name: '出勤率'}]}]
+      series: [{name: '', type: 'gauge', detail: {formatter: '{value}%'}, data: [{value: 0, name: '实时出勤率'}]}]
     };
 
     const echartsRoom = echarts.init(this.echartsRoomElementRef.nativeElement);
@@ -85,10 +123,6 @@ export class StatisticsComponent implements OnInit, OnDestroy {
       this.getLeaveChartLive(date, echartsLeave, leaveOption);
       this.getClassComingChart(date, echartsAttendance, classComingOption);
     }, 5000);
-
-    window.onresize = () => {
-      this.echartsInstances.forEach((item) => item.resize());
-    };
   }
 
   setOption(key: string, sum: string, instance: echarts.ECharts, option: any) {
@@ -121,7 +155,7 @@ export class StatisticsComponent implements OnInit, OnDestroy {
   }
 
   getApartmentInfoChart() {
-    const option: echarts.EChartOption | echarts.EChartsResponsiveOption = {
+    const option: echarts.EChartOption = {
       title: {text: '公寓人数', subtext: '', left: 'center'},
       tooltip: {trigger: 'item', formatter: '{a} <br/>{b} : {c} ({d}%)'},
       legend: {orient: 'vertical', left: 'left', data: []},
@@ -148,46 +182,24 @@ export class StatisticsComponent implements OnInit, OnDestroy {
   }
 
   getCounselorAllChart(startDate: string, endDate: string) {
-    const option: echarts.EChartOption | echarts.EChartsResponsiveOption = {
-      title: {text: '各辅导员统计信息'},
-      tooltip: {trigger: 'axis', axisPointer: {type: 'shadow'}},
-      legend: {data: ['寝室归寝', '课堂出勤', '请假']},
-      grid: {left: '3%', right: '4%', bottom: '3%', containLabel: true},
-      xAxis: {type: 'value', boundaryGap: [0, 0.01]},
-      yAxis: {type: 'category'},
-      series: [{name: '寝室归寝', type: 'bar', data: []}, {name: '课堂出勤', type: 'bar', data: []}, {name: '请假', type: 'bar', data: []}]
-    };
-    const counselorEcharts = echarts.init(this.counselorElementRef.nativeElement);
     this.statisticsService.getAllCounselorChart(startDate, endDate).subscribe((allCounselors) => {
-      (option.yAxis as echarts.EChartOption.YAxis).data = allCounselors.map((item) => item.user.name);
-      option.series[0].data = allCounselors.map((item) => this.percentage(item.homeComing.coming, item.homeComing.sum));
-      option.series[1].data = allCounselors.map((item) => this.percentage(item.classComing.coming, item.classComing.sum));
-      option.series[2].data = allCounselors.map((item) => this.percentage(item.leave.leave, item.leave.sum));
-      counselorEcharts.setOption(option, true);
+      (this.counselorAllChartOption.yAxis as echarts.EChartOption.YAxis).data =
+        allCounselors.map((item) => item.user.name);
+      this.counselorAllChartOption.series[0].data =
+        allCounselors.map((item) => this.percentage(item.homeComing.coming, item.homeComing.sum));
+      this.counselorAllChartOption.series[1].data =
+        allCounselors.map((item) => this.percentage(item.classComing.coming, item.classComing.sum));
+      this.counselorAllChartOption.series[2].data =
+        allCounselors.map((item) => this.percentage(item.leave.leave, item.leave.sum));
+      this.counselorEcharts.setOption(this.counselorAllChartOption, true);
     });
   }
 
   getPolylineData(startDate: dayjs.Dayjs, endDate: dayjs.Dayjs) {
-    const option: echarts.EChartOption | echarts.EChartsResponsiveOption = {
-      title: {text: '统计折线图'},
-      tooltip: {trigger: 'axis'},
-      dataZoom: [{type: 'slider', start: 0, end: 100}, {type: 'inside', start: 0, end: 100}],
-      legend: {data: ['寝室归寝', '课堂出勤', '请假']},
-      grid: {left: '3%', right: '4%', bottom: '3%', containLabel: true},
-      xAxis: {type: 'category', boundaryGap: false, data: []},
-      yAxis: {type: 'value'},
-      series: [
-        {name: '寝室归寝', type: 'line', smooth: true, data: []},
-        {name: '课堂出勤', type: 'line', smooth: true, data: []},
-        {name: '请假', type: 'line', smooth: true, data: []}
-      ]
-    };
-
-    (option.xAxis as echarts.EChartOption.XAxis).data = [];
-    option.series[0].data = [];
-    option.series[1].data = [];
-    option.series[2].data = [];
-    const polylineEcharts = echarts.init(this.polylineElementRef.nativeElement);
+    (this.polylineOption.xAxis as echarts.EChartOption.XAxis).data = [];
+    this.polylineOption.series[0].data = [];
+    this.polylineOption.series[1].data = [];
+    this.polylineOption.series[2].data = [];
 
     const allLeaveRequest: Observable<{ index: number, value: Leave }>[] = [];
     const allClassComingRequest: Observable<{ index: number, value: ClassComing }>[] = [];
@@ -196,23 +208,23 @@ export class StatisticsComponent implements OnInit, OnDestroy {
     const day = endDate.diff(startDate, 'day') + 1;
     for (let i = 0; i < day; i++) {
       const eachDay = dayjs(startDate).add(i, 'day').format('YYYY-MM-DD');
-      (option.xAxis as echarts.EChartOption.XAxis).data.push(eachDay);
+      (this.polylineOption.xAxis as echarts.EChartOption.XAxis).data.push(eachDay);
       allHomeComingRequest.push(this.statisticsService.getHomeComingChart(eachDay).pipe(map((home) => ({index: i, value: home}))));
       allClassComingRequest.push(this.statisticsService.getClassComingChart(eachDay).pipe(map((clazz) => ({index: i, value: clazz}))));
       allLeaveRequest.push(this.statisticsService.getLeaveChart(eachDay).pipe(map((leave) => ({index: i, value: leave}))));
     }
 
     merge(...allHomeComingRequest).subscribe((data) => {
-      this.add2Array(option.series[0].data as number[], data.index, this.percentage(data.value.coming, data.value.sum));
-      polylineEcharts.setOption(option, true);
+      this.add2Array(this.polylineOption.series[0].data as number[], data.index, this.percentage(data.value.coming, data.value.sum));
+      this.polylineEcharts.setOption(this.polylineOption, true);
     });
     merge(...allClassComingRequest).subscribe((data) => {
-      this.add2Array(option.series[1].data as number[], data.index, this.percentage(data.value.coming, data.value.sum));
-      polylineEcharts.setOption(option, true);
+      this.add2Array(this.polylineOption.series[1].data as number[], data.index, this.percentage(data.value.coming, data.value.sum));
+      this.polylineEcharts.setOption(this.polylineOption, true);
     });
     merge(...allLeaveRequest).subscribe((data) => {
-      this.add2Array(option.series[2].data as number[], data.index, this.percentage(data.value.leave, data.value.sum));
-      polylineEcharts.setOption(option, true);
+      this.add2Array(this.polylineOption.series[2].data as number[], data.index, this.percentage(data.value.leave, data.value.sum));
+      this.polylineEcharts.setOption(this.polylineOption, true);
     });
   }
 
@@ -225,5 +237,12 @@ export class StatisticsComponent implements OnInit, OnDestroy {
 
   add2Array<T>(data: Array<T>, index: number, value: T): void {
     data.splice(index, 0, value);
+  }
+
+  onDateChange(dates: Date[]) {
+    const start = dayjs(dates[0]);
+    const end = dayjs(dates[1]);
+    this.getCounselorAllChart(start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD'));
+    this.getPolylineData(start, end);
   }
 }
