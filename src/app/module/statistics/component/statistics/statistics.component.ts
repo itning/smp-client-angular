031128807655a719b7203d70817ccd50d4ced956
itning <itning@itning.top>
2020-04-23 18:14:36 +0800

    feat: counselor chart

--- src/app/module/statistics/component/statistics/statistics.component.ts
+++ src/app/module/statistics/component/statistics/statistics.component.ts
@@ -2,6 +2,10 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import * as echarts from 'echarts';
 import {StatisticsService} from '../../../../service/statistics.service';
 import {DatePipe} from '@angular/common';
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+
+dayjs.locale('zh-cn');
 
 @Component({
   selector: 'app-statistics',
@@ -17,6 +21,8 @@ export class StatisticsComponent implements OnInit {
   echartsAttendanceElementRef: ElementRef;
   @ViewChild('apartment', {static: true})
   apartmentElementRef: ElementRef;
+  @ViewChild('counselor', {static: true})
+  counselorElementRef: ElementRef;
 
   echartsInstances: echarts.ECharts[] = [];
   datePipe = new DatePipe('zh-Hans');
@@ -27,6 +33,7 @@ export class StatisticsComponent implements OnInit {
   ngOnInit(): void {
     this.initLiveChart();
     this.getApartmentInfoChart();
+    this.getCounselorAllChart(dayjs().subtract(1, 'month').format('YYYY-MM-DD'), dayjs().format('YYYY-MM-DD'));
   }
 
   initLiveChart() {
@@ -73,7 +80,6 @@ export class StatisticsComponent implements OnInit {
     return (data) => {
       if (data[sum] === 0) {
         option.series[0].name = '0/0';
-        option.series[0].data[0].value = 0;
       } else {
         option.series[0].name = `${data[key]}/${data[sum]}`;
         option.series[0].data[0].value = Number(((data[key] / data[sum]) * 100).toFixed(2));
@@ -105,36 +111,17 @@ export class StatisticsComponent implements OnInit {
 
   getApartmentInfoChart() {
     const option: echarts.EChartOption | echarts.EChartsResponsiveOption = {
-      title: {
-        text: '公寓人数',
-        subtext: '',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: []
-      },
-      series: [
-        {
-          name: '公寓信息',
-          type: 'pie',
-          radius: '55%',
-          center: ['50%', '60%'],
-          data: [],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }
-      ]
+      title: {text: '公寓人数', subtext: '', left: 'center'},
+      tooltip: {trigger: 'item', formatter: '{a} <br/>{b} : {c} ({d}%)'},
+      legend: {orient: 'vertical', left: 'left', data: []},
+      series: [{
+        name: '公寓信息',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: [],
+        emphasis: {itemStyle: {shadowBlur: 10, shadowOffsetX: 0, shadowColor: 'rgba(0, 0, 0, 0.5)'}}
+      }]
     };
 
     const apartmentEcharts = echarts.init(this.apartmentElementRef.nativeElement);
@@ -146,6 +133,26 @@ export class StatisticsComponent implements OnInit {
       option.series[0].data = apartmentStatistics.map((item => ({name: item.name, value: item.people})));
       option.legend.data = apartmentStatistics.map((item) => item.name);
       apartmentEcharts.setOption(option, true);
+    });
+  }
+
+  getCounselorAllChart(startDate: string, endDate: string) {
+    const option: echarts.EChartOption | echarts.EChartsResponsiveOption = {
+      title: {text: '各辅导员统计信息'},
+      tooltip: {trigger: 'axis', axisPointer: {type: 'shadow'}},
+      legend: {data: ['寝室归寝', '课堂出勤', '请假']},
+      grid: {left: '3%', right: '4%', bottom: '3%', containLabel: true},
+      xAxis: {type: 'value', boundaryGap: [0, 0.01]},
+      yAxis: {type: 'category'},
+      series: [{name: '寝室归寝', type: 'bar', data: []}, {name: '课堂出勤', type: 'bar', data: []}, {name: '请假', type: 'bar', data: []}]
+    };
+    const counselorEcharts = echarts.init(this.counselorElementRef.nativeElement);
+    this.statisticsService.getAllCounselorChart(startDate, endDate).subscribe((allCounselors) => {
+      (option.yAxis as echarts.EChartOption.YAxis).data = allCounselors.map((item) => item.user.name);
+      option.series[0].data = allCounselors.map((item) => Number(((item.homeComing.coming / item.leave.sum) * 100).toFixed(2)));
+      option.series[1].data = allCounselors.map((item) => Number(((item.classComing.coming / item.leave.sum) * 100).toFixed(2)));
+      option.series[2].data = allCounselors.map((item) => Number(((item.leave.leave / item.leave.sum) * 100).toFixed(2)));
+      counselorEcharts.setOption(option, true);
     });
   }
 }
